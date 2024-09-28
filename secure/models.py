470f5a14ac5ed35d139084799f0894247aa522d1
GodsplanELot <elotgodsplan2@gmail.com>
2024-09-28 02:49:28 +0100

    Just finished creating user model and additional fields, about to makemigrations againg i hope it doesnt chrash

--- secure/models.py
+++ secure/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Profile model to store user balance and other custom fields
+class UserBalance(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to the User model
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Store user balance
+
+    def __str__(self):
+        return f'{self.user.username} - Balance: {self.balance}'
